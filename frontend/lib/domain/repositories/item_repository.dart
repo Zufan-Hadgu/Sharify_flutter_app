b55a9d65866aa1c274c105f8d185a60afd5575b3
tsegaeph <tsegaephrem080@gmail.com>
2025-06-09 21:36:46 +0300

    Item repository updated

--- frontend/lib/domain/repositories/item_repository.dart
+++ frontend/lib/domain/repositories/item_repository.dart
@@ -5,6 +5,8 @@ abstract class ItemRepository {
   Future<ItemEntity?> getItemById(String id);
   Future<void> borrowItem(String itemId);
   Future<List<ItemEntity>> getBorrowedItems(String userId);
+  Future<bool> updateNote(String itemId, String note);
+
 
 
 

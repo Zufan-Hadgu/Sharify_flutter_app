f6bc3ed112d90d587d08c1cc6af5efbeef68ee21
sifen getachew <sifenmesfin10@gmail.com>
2025-06-11 01:34:53 -0700

    faliures and admin

--- frontend/lib/domain/repositories/admin_repository.dart
+++ frontend/lib/domain/repositories/admin_repository.dart
@@ -13,3 +13,27 @@ abstract class AdminRepository {
 
 
 }
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import '../../core/errors/failures.dart';
+import '../entities/dashboard_stats_entity.dart';
+import '../entities/item_entity.dart';
+
+abstract class AdminRepository {
+  Future<int> getTotalUsers();
+  Future<int> getTotalItems();
+  Future<void> addAdminItem(ItemEntity item, MultipartFile image);
+  Future<List<ItemEntity>> getAdminItems();
+  Future<void> updateItem({
+    required String itemId,
+    required ItemEntity item,
+    required MultipartFile? image, // ✅ Uses MultipartFile for image upload
+
+  });
+
+
+
+
+
+}
